b1ecfcbaaa44ced5919a91e95619ee744fdee486
ashikurahman1 <ashikurahmantuhin@gmail.com>
2025-10-22 00:04:07 +0600

    footer

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Root = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="">
+      <div className="flex-grow">
         <Outlet />
       </div>
       {/* Footer */}
+      <Footer />
     </div>
   );
 };
